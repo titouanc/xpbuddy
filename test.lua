ba8f1405002a27dcd8f05704fed6de63440ac80a
iTitou <moiandme@gmail.com>
2022-01-08 18:18:51 +0100

    Add test
    Signed-off-by: iTitou <moiandme@gmail.com>

---
+++ test.lua
@@ -0,0 +1,126 @@
+-- Mocks for Wow API
+local mock_functions = {
+    GetTime = {0},
+    UnitXP = {0},
+    UnitXPMax = {100},
+    GetInstanceInfo = {"Azeroth"},
+    GetPetExperience = {0, 100},
+    IsInInstance = {false},
+}
+
+for func_name, default_value in pairs(mock_functions) do
+    _G[func_name .. "_VALUE"] = default_value
+    _G[func_name] = function (...)
+        return table.unpack(_G[func_name .. "_VALUE"])
+    end
+end
+
+SlashCmdList = {}
+
+function CreateFrame()
+    return {
+        RegisterEvent = function() end,
+        SetScript = function() end,
+    }
+end
+
+-- Load our Addon resources
+require('meter')
+local Addon = require('main')
+
+-- Tests
+local function assertEqual(expected, actual)
+    if expected ~= actual then
+        error("Assertion failed: " .. expected .. " != " .. actual, 2)
+    end
+end
+
+function test_gain_player_xp_in_world()
+    Addon:PLAYER_ENTERING_WORLD()
+    assertEqual(0, Addon.session.total_gained_xp)
+    assertEqual(0, #Addon.instances)
+    assertEqual(0, #Addon.pets)
+
+    -- "Kill 1 mob for 50xp" (lvl1=100xp)
+    GetTime_VALUE = {10}
+    UnitXP_VALUE = {50}
+    Addon:PLAYER_XP_UPDATE()
+    -- We should be at 50% of the level, and gained 50xp in total
+    assertEqual(50, Addon.session.total_gained_xp)
+    assertEqual(0.5, Addon.session.total_gained_lvl)
+    assertEqual(10, Addon.session:totalTime())
+
+    -- "Turn in quest for 80xp" (lvl2=200xp)
+    GetTime_VALUE = {20}
+    UnitXP_VALUE = {30}
+    UnitXPMax_VALUE = {200}
+    Addon:PLAYER_XP_UPDATE()
+    -- We should be at 15% of the next level, and gained 130xp in total
+    assertEqual(130, Addon.session.total_gained_xp)
+    assertEqual(1.15, Addon.session.total_gained_lvl)
+    assertEqual(20, Addon.session:totalTime())
+
+    assertEqual(0, #Addon.instances)
+    assertEqual(0, #Addon.pets)
+end
+
+function test_gain_player_xp_in_instance()
+    IsInInstance_VALUE = {true}
+    GetInstanceInfo_VALUE = {"BRD"}
+
+    Addon:PLAYER_ENTERING_WORLD()
+    assertEqual(0, Addon.session.total_gained_xp)
+    assert(Addon.instances["BRD"] ~= nil, "Expected to have instance BRD")
+    assertEqual(Addon.instances["BRD"], Addon.current_instance)
+
+    -- "Kill 1 mob for 30xp" (lvl1=100xp)
+    GetTime_VALUE = {10}
+    UnitXP_VALUE = {30}
+    Addon:PLAYER_XP_UPDATE()
+    -- Both the session XP and the instance XP should be increased
+    assertEqual(30, Addon.session.total_gained_xp)
+    assertEqual(0.3, Addon.session.total_gained_lvl)
+    assertEqual(10, Addon.session:totalTime())
+    assertEqual(30, Addon.current_instance.total_gained_xp)
+    assertEqual(0.3, Addon.current_instance.total_gained_lvl)
+    assertEqual(10, Addon.current_instance:totalTime())
+
+    -- Get out of instance
+    GetTime_VALUE = {20}
+    IsInInstance_VALUE = {false}
+    GetInstanceInfo_VALUE = {"Azeroth"}
+    Addon:PLAYER_ENTERING_WORLD()
+    assertEqual(nil, Addon.current_instance)
+    assertEqual(20, Addon.session:totalTime())
+    assertEqual(20, Addon.instances["BRD"]:totalTime())
+
+    -- "Kill 1 mob for 30xp" (lvl1=100xp)
+    GetTime_VALUE = {30}
+    UnitXP_VALUE = {60}
+    Addon:PLAYER_XP_UPDATE()
+    -- Only the session XP should be increased
+    assertEqual(60, Addon.session.total_gained_xp)
+    assertEqual(0.6, Addon.session.total_gained_lvl)
+    assertEqual(30, Addon.session:totalTime())
+    assertEqual(30, Addon.instances["BRD"].total_gained_xp)
+    assertEqual(0.3, Addon.instances["BRD"].total_gained_lvl)
+    assertEqual(20, Addon.instances["BRD"]:totalTime())
+end
+
+-- Collect all functions test_...() like pytest does
+for func_name, func in pairs(_G) do
+    Addon:init()
+    for func_name, default_value in pairs(mock_functions) do
+        _G[func_name .. "_VALUE"] = default_value
+    end
+    if func_name:sub(1, 5) == "test_" then
+        local status_text
+        local ok, retval = pcall(func)
+        if ok then
+            status_text = "\x1b[32mOK\x1b[0m"
+        else
+            status_text = "\x1b[31mFAIL\x1b[37m (" .. retval .. ")\x1b[0m"
+        end
+        print(func_name, status_text)
+    end
+end
