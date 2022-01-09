8c0550f336a7351a3d8f05983053de4e56a31f6d
iTitou <moiandme@gmail.com>
2022-01-09 15:20:54 +0100

    Intercept prints in tests + improve runtest output
    Signed-off-by: iTitou <moiandme@gmail.com>

--- runtests.lua
+++ runtests.lua
@@ -1,3 +1,9 @@
+local builtin_print = print
+
+Console = {}
+
+print = function (...) table.insert(Console, {...}) end
+
 -- Compat Lua5.3 / Lua 5.1
 if not unpack then unpack = table.unpack end
 
@@ -38,30 +44,36 @@ function assertEqual(expected, actual)
     end
 end
 
+local function beforeTestFunction()
+    Console = {}
+    for func_name, default_value in pairs(mock_functions) do
+        _G[func_name .. "_VALUE"] = default_value
+    end
+    Addon:init()
+end
+
 -- Collect all functions test_...() like pytest does
 local function runTests()
     local failed = false
 
-    for filename in io.popen('dir tests'):lines() do
+    -- Find all files containing tests
+    for filename in io.popen('ls tests'):lines() do
         -- require without the ".lua" extension
-        require('tests/' .. filename:sub(1, filename:len() - 4))
+        require('tests/' .. filename:gsub(".lua$", ""))
     end
 
     for func_name, func in pairs(_G) do
-        Addon:init()
-        for func_name, default_value in pairs(mock_functions) do
-            _G[func_name .. "_VALUE"] = default_value
-        end
+        -- Find all functions in the form `test_...()`
         if func_name:sub(1, 5) == "test_" then
+            beforeTestFunction()
             local status_text
             local ok, retval = pcall(func)
             if ok then
-                status_text = "\x1b[32mOK\x1b[0m"
+                builtin_print("\x1b[32m OK \x1b[0m " .. func_name)
             else
-                status_text = "\x1b[31mFAIL\x1b[37m (" .. retval .. ")\x1b[0m"
+                builtin_print("\x1b[31mFAIL\x1b[0m " .. func_name ..  " \x1b[37m(" .. retval .. ")\x1b0m")
                 failed = true
             end
-            print(func_name, status_text)
         end
     end
 
